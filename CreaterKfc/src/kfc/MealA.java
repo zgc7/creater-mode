f7a88fc1952b32da5ea3c58cc91ea58a8ab8e0f9
unknown <>
2017-05-07 22:01:05 +0800

    this is my first work

---
+++ CreaterKfc/src/kfc/MealA.java
@@ -0,0 +1,13 @@
+package kfc;
+
+public class MealA extends MealBuilder{  
+	  
+    public void buildDrink() {  
+        meal.setDrink("一杯可乐");  
+    }  
+  
+    public void buildFood() {  
+        meal.setFood("一盒薯条");  
+    }  
+  
+}  
---
+++ CreaterKfc/src/kfc/MealB.java
@@ -0,0 +1,13 @@
+package kfc;
+
+public class MealB extends MealBuilder{  
+  
+    public void buildDrink() {  
+        meal.setDrink("一杯柠檬果汁");  
+    }  
+  
+    public void buildFood() {  
+        meal.setFood("三个鸡翅");  
+    }  
+  
+}  
